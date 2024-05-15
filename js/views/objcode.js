0f12e9bcec19bbf01f808f90b157229d9247c4ed
Wenhan Lyu <lvwh0330@gmail.com>
2024-05-15 20:42:57 +0800

    feat: explain button that can get object code and register info

--- js/views/objcode.js
+++ js/views/objcode.js
@@ -1,128 +1,163 @@
 var ObjectCodeView = Backbone.View.extend({
-	className: 'object-code',
+    className: 'object-code',
 
-	initialize: function (options) {
-		this.template = _.template($('#tmpl_object_code').html());
-		this.code = options.code;
-		this.highlightedLines = {};
-		this.initLines();
-		this.listenTo(Backbone.Events, 'app:redraw', this.highlightCurrentLine);
-		this.render();
-	},
+    events: {
+        'click .explain-btn': 'printCurrentCodeState'
+    },
 
-	render: function () {
-		if (!this.rendered) {
-			this.$el.empty().append(this.template());
-			this.rendered = true;
-			this.$lineContainer = this.$('.lines');
-		}
+    initialize: function (options) {
+        this.template = _.template($('#tmpl_object_code').html());
+        this.code = options.code;
+        this.highlightedLines = {};
+        this.initLines();
+        this.listenTo(Backbone.Events, 'app:redraw', this.highlightCurrentLine);
+        this.render();
+    },
 
-		this.$lineContainer.empty().append(_.map(this.$lines, function ($line) {
-			return $line.$el;
-		}));
-	},
+    render: function () {
+        if (!this.rendered) {
+            this.$el.empty().append(this.template());
+            this.rendered = true;
+            this.$lineContainer = this.$('.lines');
+        }
 
-	setObjectCode: function (code) {
-		this.code = code;
-		this.initLines();
-		this.render();
-	},
+        this.$lineContainer.empty().append(_.map(this.$lines, function ($line) {
+            return $line.$el;
+        }));
+    },
 
-	highlightCurrentLine: function () {
-		var linesToHighlight = this.linesByLineNo[PC.toInt()] || [];
-		var linesToUnhighlight = this.highlightedLines;
+    setObjectCode: function (code) {
+        this.code = code;
+        this.initLines();
+        this.render();
+    },
 
-		_.each(linesToUnhighlight, function ($line) {
-			$line.unhighlight();
-		});
+    highlightCurrentLine: function () {
+        var linesToHighlight = this.linesByLineNo[PC.toInt()] || [];
+        var linesToUnhighlight = this.highlightedLines;
 
-		_.each(linesToHighlight, function ($line) {
-			$line.highlight();
-		});
+        _.each(linesToUnhighlight, function ($line) {
+            $line.unhighlight();
+        });
 
-		this.highlightedLines = linesToHighlight;
+        _.each(linesToHighlight, function ($line) {
+            $line.highlight();
+        });
 
-		// scroll into view
-		var lineToScrollTo = linesToHighlight[linesToHighlight.length - 1];
-		if (!lineToScrollTo)
-			return;
+        this.highlightedLines = linesToHighlight;
 
-		var $lineToScrollTo = lineToScrollTo.$el;
+        // Scroll to the line if it is not in view
+        this.scrollToLine(linesToHighlight[linesToHighlight.length - 1]);
 
-		var top = $lineToScrollTo.position().top;
-		if (top < 0)
-			this.$lineContainer.scrollTop($lineToScrollTo.index() * 15 - 40);
-		else {
-			var linesHeight = this.$lineContainer.height();
-			if (top > linesHeight - 15)
-				this.$lineContainer.scrollTop($lineToScrollTo.index() * 15 - linesHeight + 55);
-		}
-	},
+        // Print the current state of the code, including errors
+        // this.printCurrentCodeState();
+    },
 
-	initLines: function () {
-		this.$lines = [];
-		this.linesByLineNo = {};
+    scrollToLine: function ($line) {
+        if (!$line) return;
+        var $lineToScrollTo = $line.$el;
+        var top = $lineToScrollTo.position().top;
+        if (top < 0)
+            this.$lineContainer.scrollTop($lineToScrollTo.index() * 15 - 40);
+        else {
+            var linesHeight = this.$lineContainer.height();
+            if (top > linesHeight - 15)
+                this.$lineContainer.scrollTop($lineToScrollTo.index() * 15 - linesHeight + 55);
+        }
+    },
 
-		if (this.code)
-			if (this.code.errors.length === 0)
-				_.each(this.code.obj, function (line) {
-					var $line = new ObjectCodeLineView({line: line});
-					this.$lines.push($line);
-					var lineno = line.substring(2, 8).trim();
-					var binary = line.substring(9, 22).trim();
-					if (lineno && binary.trim().length) {
-						var lineno = parseInt(lineno, 16);
-						if (this.linesByLineNo[lineno])
-							this.linesByLineNo[lineno].push($line);
-						else
-							this.linesByLineNo[lineno] = [$line];
-					}
-				}, this);
-			else
-				_.each(this.code.errors, function (error) {
-					var $line = new ObjectCodeLineErrorView({ error: error });
-					this.$lines.push($line);
-				}, this);
-	}
+    printCurrentCodeState: function () {
+        var self = this;
+        console.clear();
+        _.each(this.$lines, function ($line, index) {
+            var isHighlighted = $line.$el.hasClass('highlighted');
+            var isError = $line.$el.hasClass('object-code-line-error');
+            var lineText = $line.$el.text();
+
+            if (isHighlighted && isError) {
+                console.log('<Error Current> ' + lineText + ' </Error Current>');
+            } else if (isError) {
+                console.log('<Error> ' + lineText + ' </Error>');
+            } else if (isHighlighted) {
+                console.log('<Current> ' + lineText + ' </Current>');
+            } else {
+                console.log(lineText);
+            }
+        });
+        vent.trigger('log:registers');
+    },
+
+    initLines: function () {
+        this.$lines = [];
+        this.linesByLineNo = {};
+
+        if (this.code)
+            if (this.code.errors.length === 0)
+                _.each(this.code.obj, function (line) {
+                    var $line = new ObjectCodeLineView({line: line});
+                    this.$lines.push($line);
+                    var lineno = line.substring(2, 8).trim();
+                    var binary = line.substring(9, 22).trim();
+                    if (lineno && binary.trim().length) {
+                        var lineno = parseInt(lineno, 16);
+                        if (this.linesByLineNo[lineno])
+                            this.linesByLineNo[lineno].push($line);
+                        else
+                            this.linesByLineNo[lineno] = [$line];
+                    }
+                }, this);
+            else
+                _.each(this.code.errors, function (error) {
+                    var $line = new ObjectCodeLineErrorView({error: error});
+                    this.$lines.push($line);
+                }, this);
+    }
 });
 
 var ObjectCodeLineView = Backbone.View.extend({
-	className: 'object-code-line',
+    className: 'object-code-line',
 
-	initialize: function (options) {
-		this.updateSource(options);
-		this.render();
-	},
+    initialize: function (options) {
+        this.updateSource(options);
+        this.render();
+    },
 
-	updateSource: function (options) {
-		this.line = options.line;
-	},
+    updateSource: function (options) {
+        this.line = options.line;
+    },
 
-	render: function () {
-		this.$el.text(this.line);
-	},
+    render: function () {
+        this.$el.text(this.line);
+    },
 
-	highlight: function () {
-		this.$el.addClass('highlighted');
-	},
-	unhighlight: function () {
-		this.$el.removeClass('highlighted');
-	}
+    highlight: function () {
+        this.$el.addClass('highlighted');
+    },
+    unhighlight: function () {
+        this.$el.removeClass('highlighted');
+    }
 });
 
 var ObjectCodeLineErrorView = Backbone.View.extend({
-	className: 'object-code-line object-code-line-error',
-	template: _.template($('#tmpl_object_code_error').html()),
+    className: 'object-code-line object-code-line-error',
+    template: _.template($('#tmpl_object_code_error').html()),
 
-	initialize: function (options) {
-		this.options = {
-			lineno: options.error[0],
-			source: options.error[1]
-		}
-		this.render();
-	},
+    initialize: function (options) {
+        this.options = {
+            lineno: options.error[0],
+            source: options.error[1]
+        }
+        this.render();
+    },
 
-	render: function () {
-		this.$el.empty().append(this.template(this.options));
-	}
+    render: function () {
+        this.$el.empty().append(this.template(this.options));
+    },
+
+    highlight: function () {
+        this.$el.addClass('highlighted');
+    },
+    unhighlight: function () {
+        this.$el.removeClass('highlighted');
+    }
 });
--- js/views/registers.js
+++ js/views/registers.js
@@ -1,53 +1,71 @@
+var vent = _.extend({}, Backbone.Events);
+
+
 var RegistersView = Backbone.View.extend({
-	className: 'registers',
+    className: 'registers',
 
-	initialize: function () {
-		this.template = _.template($('#tmpl_registers').html());
-		this.render();
-	},
+    initialize: function () {
+        this.template = _.template($('#tmpl_registers').html());
+        this.render();
+        _.bindAll(this, 'logRegisters');
+        vent.on('log:registers', this.logRegisters);
+    },
 
-	render: function () {
-		var registers = {
-			rax_hex: '0x' + padHex(REG[0].toUnsigned().toString(16), 16),
-			rax_dec: REG[0].toSigned().toString(10),
-			rcx_hex: '0x' + padHex(REG[1].toUnsigned().toString(16), 16),
-			rcx_dec: REG[1].toSigned().toString(10),
-			rdx_hex: '0x' + padHex(REG[2].toUnsigned().toString(16), 16),
-			rdx_dec: REG[2].toSigned().toString(10),
-			rbx_hex: '0x' + padHex(REG[3].toUnsigned().toString(16), 16),
-			rbx_dec: REG[3].toSigned().toString(10),
-			rsp_hex: '0x' + padHex(REG[4].toUnsigned().toString(16), 16),
-			rsp_dec: REG[4].toSigned().toString(10),
-			rbp_hex: '0x' + padHex(REG[5].toUnsigned().toString(16), 16),
-			rbp_dec: REG[5].toSigned().toString(10),
-			rsi_hex: '0x' + padHex(REG[6].toUnsigned().toString(16), 16),
-			rsi_dec: REG[6].toSigned().toString(10),
-			rdi_hex: '0x' + padHex(REG[7].toUnsigned().toString(16), 16),
-			rdi_dec: REG[7].toSigned().toString(10),
+    render: function () {
+        this.registers = {
+            rax_hex: '0x' + padHex(REG[0].toUnsigned().toString(16), 16),
+            rax_dec: REG[0].toSigned().toString(10),
+            rcx_hex: '0x' + padHex(REG[1].toUnsigned().toString(16), 16),
+            rcx_dec: REG[1].toSigned().toString(10),
+            rdx_hex: '0x' + padHex(REG[2].toUnsigned().toString(16), 16),
+            rdx_dec: REG[2].toSigned().toString(10),
+            rbx_hex: '0x' + padHex(REG[3].toUnsigned().toString(16), 16),
+            rbx_dec: REG[3].toSigned().toString(10),
+            rsp_hex: '0x' + padHex(REG[4].toUnsigned().toString(16), 16),
+            rsp_dec: REG[4].toSigned().toString(10),
+            rbp_hex: '0x' + padHex(REG[5].toUnsigned().toString(16), 16),
+            rbp_dec: REG[5].toSigned().toString(10),
+            rsi_hex: '0x' + padHex(REG[6].toUnsigned().toString(16), 16),
+            rsi_dec: REG[6].toSigned().toString(10),
+            rdi_hex: '0x' + padHex(REG[7].toUnsigned().toString(16), 16),
+            rdi_dec: REG[7].toSigned().toString(10),
+            r8_hex: '0x' + padHex(REG[8].toUnsigned().toString(16), 16),
+            r8_dec: REG[8].toSigned().toString(10),
+            r9_hex: '0x' + padHex(REG[9].toUnsigned().toString(16), 16),
+            r9_dec: REG[9].toSigned().toString(10),
+            r10_hex: '0x' + padHex(REG[10].toUnsigned().toString(16), 16),
+            r10_dec: REG[10].toSigned().toString(10),
+            r11_hex: '0x' + padHex(REG[11].toUnsigned().toString(16), 16),
+            r11_dec: REG[11].toSigned().toString(10),
+            r12_hex: '0x' + padHex(REG[12].toUnsigned().toString(16), 16),
+            r12_dec: REG[12].toSigned().toString(10),
+            r13_hex: '0x' + padHex(REG[13].toUnsigned().toString(16), 16),
+            r13_dec: REG[13].toSigned().toString(10),
+            r14_hex: '0x' + padHex(REG[14].toUnsigned().toString(16), 16),
+            r14_dec: REG[14].toSigned().toString(10),
+            sf: SF,
+            zf: ZF,
+            of: OF,
+            stat: STAT,
+            err: ERR,
+            pc: '0x' + padHex(PC.toString(16), 4)
+        };
 
-			r8_hex: '0x' + padHex(REG[8].toUnsigned().toString(16), 16),
-			r8_dec: REG[8].toSigned().toString(10),
-			r9_hex: '0x' + padHex(REG[9].toUnsigned().toString(16), 16),
-			r9_dec: REG[9].toSigned().toString(10),
-			r10_hex: '0x' + padHex(REG[10].toUnsigned().toString(16), 16),
-			r10_dec: REG[10].toSigned().toString(10),
-			r11_hex: '0x' + padHex(REG[11].toUnsigned().toString(16), 16),
-			r11_dec: REG[11].toSigned().toString(10),
-			r12_hex: '0x' + padHex(REG[12].toUnsigned().toString(16), 16),
-			r12_dec: REG[12].toSigned().toString(10),
-			r13_hex: '0x' + padHex(REG[13].toUnsigned().toString(16), 16),
-			r13_dec: REG[13].toSigned().toString(10),
-			r14_hex: '0x' + padHex(REG[14].toUnsigned().toString(16), 16),
-			r14_dec: REG[14].toSigned().toString(10),
+        this.$el.empty().append(this.template(this.registers));
 
-			sf: SF,
-			zf: ZF,
-			of: OF,
-			stat: STAT,
-			err: ERR,
-			pc: '0x' + padHex(PC.toString(16), 4)
-		};
+        // Call the separate function to log the registers
+        // this.logRegisters();
+    },
 
-		this.$el.empty().append(this.template(registers));
-	}
+    logRegisters: function () {
+        console.log('Register Values and Flags:');
+        Object.keys(this.registers).forEach((key) => {
+            console.log(`${key}: ${this.registers[key]}`);
+        });
+    },
+
+    remove: function () {
+        vent.off('log:registers', this.logRegisters);
+        Backbone.View.prototype.remove.call(this);
+    }
 });
